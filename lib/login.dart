2adf0bf8e8c704b897a7e5ec11afeda728558dda
Ajesh Ajayan <ajesh@xactsolutions.com>
2020-09-21 22:15:21 +0530

    first commit

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  Color primaryColor = new Color(0xFF0024c3);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,111 +17,173 @@ class _LoginState extends State<Login> {
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/images/bermuda-welcome.png"),
+              image: AssetImage("assets/images/login_bg.jpg"),
               fit: BoxFit.cover,
             ),
           ),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(bottom: 30),
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: TextFormField(
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0x50FEFEFE),
-                        suffixIcon: Icon(
-                          Icons.person,
-                          color: Colors.green,
-                        ),
-                        border: const OutlineInputBorder(),
-                        labelText: "Username",
-                        labelStyle: TextStyle(color: Colors.green),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.green,
+            child: FractionallySizedBox(
+              heightFactor: 0.75,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: TextFormField(
+                        cursorColor: primaryColor,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x50FEFEFE),
+                          suffixIcon: Icon(
+                            Icons.person,
+                            color: primaryColor,
                           ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.green,
+                          border: const OutlineInputBorder(),
+                          labelText: "Username",
+                          labelStyle: TextStyle(color: primaryColor),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: TextFormField(
-                      obscureText: true,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color(0x50FFFFFF),
-                        suffixIcon: Icon(
-                          Icons.lock,
-                          color: Colors.green,
-                        ),
-                        border: OutlineInputBorder(),
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          color: Colors.green,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.green,
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.green,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 30),
-                  child: FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: RaisedButton(
-                            color: Colors.green,
-                            child: Container(
-                              height: 50,
-                              child: Center(
-                                child: Text(
-                                  "Log in",
-                                  style: TextStyle(
-                                    color: Colors.white,
+                  Container(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: TextFormField(
+                        obscureText: true,
+                        cursorColor: primaryColor,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0x50FFFFFF),
+                          suffixIcon: Icon(
+                            Icons.lock,
+                            color: primaryColor,
+                          ),
+                          border: OutlineInputBorder(),
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            color: primaryColor,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    child: FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: RaisedButton(
+                              color: primaryColor,
+                              child: Container(
+                                height: 50,
+                                child: Center(
+                                  child: Text(
+                                    "Log in",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
                                   ),
                                 ),
                               ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => AppHomeScreen(),
+                                  ),
+                                );
+                              },
                             ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (_) => AppHomeScreen(),
-                                ),
-                              );
-                            },
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                )
-              ],
+                  FractionallySizedBox(
+                    widthFactor: 0.8,
+                    child: Divider(
+                      color: primaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: primaryColor,
+                      child: FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Login with',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                  top: 10,
+                                  left: 3,
+                                  bottom: 10,
+                                ),
+                                child: Image(
+                                  image: AssetImage(
+                                    "assets/images/google_icon_logo.jpg",
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Not a user. ',
+                          style: TextStyle(color: primaryColor),
+                        ),
+                        InkWell(
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(color: primaryColor),
+                          ),
+                          onTap: () {},
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
